0c7806d3ddbf55153ff06b1ba62813cf2c7f385f
Igor Smirnov <uchetzapis@yahoo.com>
2023-04-05 22:47:16 +0300

    Use DatabaseModule from library

--- apps/profile/src/profile.module.ts
+++ apps/profile/src/profile.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+import { DatabaseModule } from '@app/common';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        APP_PORT: Joi.number().required(),
+      })
+    }),
+    DatabaseModule,
+  ],
   controllers: [ProfileController],
   providers: [ProfileService],
 })
---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [
+        ],
+      }),
+      inject: [ConfigService]
+  }),
+  ]
+})
+export class DatabaseModule {}
