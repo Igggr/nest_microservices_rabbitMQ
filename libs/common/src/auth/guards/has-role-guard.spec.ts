ec9783e01647959619115fd1fba6075d2479c5e6
Igor Smirnov <uchetzapis@yahoo.com>
2023-04-11 22:26:09 +0300

    Add tests for guard

--- libs/common/src/auth/guards/has-role-guard.spec.ts
+++ libs/common/src/auth/guards/has-role-guard.spec.ts
@@ -1,29 +1,50 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { HasRoleGuard } from './has-role-guard';
 import { ExecutionContext } from '@nestjs/common';
-
+import { createMock } from '@golevelup/ts-jest';
+import { Reflector } from '@nestjs/core';
 export { HasRoleGuard } from './has-role-guard';
+
 
 describe('', () => {
     let hasRoleGuard: HasRoleGuard;
-    // const context: ExecutionContext = {
-    //     // getClass() { return {}},
-    // };
+    let reflector: Reflector;
+    let mockExecutionContext
 
     beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            providers: [
-                HasRoleGuard
-            ],
-        }).compile();  // bootstrap module
+        mockExecutionContext = createMock<ExecutionContext>();
+        reflector = new Reflector();
+        hasRoleGuard = new HasRoleGuard(reflector);
+    });
 
-        hasRoleGuard =  module.get<HasRoleGuard>(HasRoleGuard)
-    })
     it('It should be defined', () => {
         expect(hasRoleGuard).toBeDefined();
     });
 
-    // it('', () => {
-    //     expect(hasRoleGuard.canActivate(context)).toBe(true);
-    // });
+    it('Allow user with role', () => {
+        reflector.getAllAndOverride = jest.fn().mockReturnValue(['ADMIN']);
+        mockExecutionContext.switchToHttp().getRequest.mockReturnValue({
+            user: {
+                id: 1,
+                roles: ['USER', 'ADMIN']
+            }
+        });
+        expect(hasRoleGuard.canActivate(mockExecutionContext)).toBe(true);
+    });
+
+    it('Will not allow user without role', () => {
+        reflector.getAllAndOverride = jest.fn().mockReturnValue(['ADMIN']);
+        mockExecutionContext.switchToHttp().getRequest.mockReturnValue({
+            user: {
+                id: 1,
+                roles: ['USER']
+            }
+        });
+        expect(hasRoleGuard.canActivate(mockExecutionContext)).toBe(false);
+    });
+
+    it('Will not allow anonymus if any role is requires', () => {
+        reflector.getAllAndOverride = jest.fn().mockReturnValue(['USER']);
+        mockExecutionContext.switchToHttp().getRequest.mockReturnValue({});
+        expect(hasRoleGuard.canActivate(mockExecutionContext)).toBe(false);
+    });
 })
---
+++ libs/common/src/auth/guards/same-user-or-has-role.spec.ts
@@ -0,0 +1,66 @@
+import { HasRoleGuard } from './has-role-guard';
+import { ExecutionContext } from '@nestjs/common';
+import { createMock } from '@golevelup/ts-jest';
+import { Reflector } from '@nestjs/core';
+import { SameUserOrHasRoleGuard } from './same-user-or-has-role-guard';
+
+
+describe('', () => {
+    let sameUserOrHasRoleGuard: SameUserOrHasRoleGuard;
+    let reflector: Reflector;
+    let mockExecutionContext
+
+    beforeEach(async () => {
+        mockExecutionContext = createMock<ExecutionContext>();
+        reflector = new Reflector();
+        sameUserOrHasRoleGuard = new SameUserOrHasRoleGuard(reflector);
+    });
+
+    it('It should be defined', () => {
+        expect(sameUserOrHasRoleGuard).toBeDefined();
+    });
+
+    it('Allow owner even without role', () => {
+        reflector.getAllAndOverride = jest.fn().mockReturnValue(['ADMIN']);
+        mockExecutionContext.switchToHttp().getRequest.mockReturnValue({
+            params: {
+                userId: 1,
+            },
+            user: {
+                id: 1,
+                roles: ['USER']
+            }
+        });
+        expect(sameUserOrHasRoleGuard.canActivate(mockExecutionContext)).toBe(true);
+    });
+
+    it('But non-owner without role shouldn\'t be allowed', () => {
+        reflector.getAllAndOverride = jest.fn().mockReturnValue(['ADMIN']);
+        mockExecutionContext.switchToHttp().getRequest.mockReturnValue({
+            params: {
+                userId: 1,
+            },
+            user: {
+                id: 2,
+                roles: ['USER']
+            }
+        });
+        expect(sameUserOrHasRoleGuard.canActivate(mockExecutionContext)).toBe(false);
+    });
+
+    it('Non-owner is allowed only if he has role', () => {
+        reflector.getAllAndOverride = jest.fn().mockReturnValue(['ADMIN']);
+        mockExecutionContext.switchToHttp().getRequest.mockReturnValue({
+            params: {
+                userId: 1,
+            },
+            user: {
+                id: 2,
+                roles: ['USER', 'ADMIN']
+            }
+        });
+        expect(sameUserOrHasRoleGuard.canActivate(mockExecutionContext)).toBe(true);
+    });
+
+
+})
