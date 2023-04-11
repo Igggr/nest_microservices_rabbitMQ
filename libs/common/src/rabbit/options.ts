7208e19ab384f231b8462dcf38e06902094489c5
Igor Smirnov <uchetzapis@yahoo.com>
2023-04-11 14:58:02 +0300

    Microservices also will be started  with compose

--- libs/common/src/rabbit/options.ts
+++ libs/common/src/rabbit/options.ts
@@ -3,7 +3,8 @@ import { Transport } from "@nestjs/microservices";
 export const OPTIONS = {
     transport: Transport.RMQ as const,
     options: {
-        urls: ['amqp://localhost:5672'],
+        // urls: ['amqp://localhost:5672'],
+        urls: ['amqp://rabbit:5672'],
         queue: 'auth_queue',
         queueOptions: {
             durable: false,
